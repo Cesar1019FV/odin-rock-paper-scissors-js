5a8b95dd8acbab20b6f308b320a155070f23fce6
Cesar Andres Fonseca Villegas <perryypipo2@gmail.com>
2023-07-26 16:50:45 -0600

    adding playRound

--- index.js
+++ index.js
@@ -7,10 +7,7 @@ function getComputerChoice() {
     let randomNumberSelection = Math.floor(Math.random() * 3);
     let result = options[randomNumberSelection];
 
-    return {
-        optionString: result,
-        optionNumber: randomNumberSelection
-      };
+    return result;
   }
 
   //this funcion normalize the input to make it the same as the option array inside getComputerChoice
@@ -35,3 +32,28 @@ function getComputerChoice() {
     }
   }
   
+  //this function starts the game, using only one round
+  function playRound(computerSelection, playerSelection) {
+    let finalResult;
+
+    console.log('Computer choice: ' + computerSelection);
+    console.log('Your choice: ' + playerSelection);
+
+    // Psoible game results depending on player and computer selections
+    if (computerSelection === playerSelection) {
+    resultado ='"Draw';
+    } else if (
+    (computerSelection === 'rock' && playerSelection === 'scissors') ||
+    (computerSelection === 'paper' && playerSelection === 'rock') ||
+    (computerSelection === 'scissors' && playerSelection === 'paper')
+    ) {
+    winner = 'Computer Wins';
+    } else {
+    winner = 'You win';
+    }
+
+    return winner;
+  }
+
+
+//this is the main code of the game
